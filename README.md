366c72e9d679fa2069b34bc5079e3b7344756918
Vedran Šego <vsego@users.noreply.github.com>
2015-06-28 03:29:24 +0100

    Create cracked-lists.py

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 Miscellaneous files
 ---
 
+* `cracked-lists.py` -- A program that automates reading of [Cracked](http://www.cracked.com/)'s list-articles by loading them and displaying only their headers.
+
 * `knight_around_board.py` -- A program that finds a way for a knight to visit all the fields on the board.
 
 * `pardoners_puzzle.py` -- A program that solves the [Pardoner's puzzle](http://math-fail.com/2015/02/the-pardoners-puzzle.html)
---
+++ cracked-lists.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+"""
+A program to retrieve [Cracked](http://www.cracked.com/)'s list-articles
+and display only their headers.
+
+Intended for fast backreading from RSS and similar collections of links.
+"""
+
+from html.parser import HTMLParser
+import urllib.request
+from pprint import pprint
+
+class CrackedListHTMLParser(HTMLParser):
+    """
+    The list parser, looking for `h1`, `h2.subheading`, and `a.next` tags.
+    """
+    first = True
+    print_data = False
+    followups = list()
+    current = list()
+    def handle_starttag(self, tag, attrs):
+        """
+        Handles the start of `h1` (the main title),
+        `h2.cubheading` (section headers),
+        and `a.next` (link to the next page).
+        """
+        attrs = dict(attrs)
+        if (tag == "h1" and self.first) or \
+           (tag == "h2" and "class" in attrs and attrs["class"] == "subheading"):
+            self.print_data = True
+            self.current = list()
+        if tag == "a" and "class" in attrs and attrs["class"] == "next" and "href" in attrs:
+            self.followups.append(attrs["href"])
+    def handle_endtag(self, tag):
+        """
+        Handles end tags (basically, wrap up the data collection).
+        """
+        if self.print_data and tag in {"h1", "h2"}:
+            self.print_data = False
+            print(" ".join(self.current))
+    def handle_data(self, data):
+        """
+        Collects the data that is inside the title tags.
+        """
+        if self.print_data:
+            self.current.append(data.strip())
+    def run(self, url = None):
+        """
+        Runs the parser. If `url` is given, it is used as a starting link.
+        """
+        if url is not None:
+            self.followups = [ url ]
+        self.first = True
+        while True:
+            urls = self.followups
+            if not urls: break
+            self.followups = list()
+            for url in urls:
+                with urllib.request.urlopen(url) as f:
+                    self.feed(f.read().decode())
+            self.first = False
+
+parser = CrackedListHTMLParser()
+
+while True:
+    url = input("\nURL (empty to quit): ")
+    if not url:
+        break
+    print()
+    parser.run(url)
